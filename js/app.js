2b09799b34faba73dc6e777e2b0225107f1d15d0
pawelkleczek <pa.vel.kleczek@gmail.com>
2017-05-09 16:27:01 +0200

    Slider done

--- js/app.js
+++ js/app.js
@@ -10,6 +10,45 @@ document.addEventListener('DOMContentLoaded', function() {
 		subMenu.classList.remove('subMenuVisible');
 	});
 
+	var products = document.querySelectorAll('[class*=product]');
+	console.log(products[0].firstElementChild);
+
+	for (var i = 0; i < products.length; i++) {
+		products[i].addEventListener('mousemove', function() {
+			this.firstElementChild.classList.add('hide');
+		});
+		products[i].addEventListener('mouseout', function() {
+			this.firstElementChild.classList.remove('hide');
+		});
+	}
+
+	var images = ['black_chair.png', 'red_chair.png', 'orange_chair.png'];
+
+	var chair = document.querySelector('#slider img');
+
+	var prev = document.getElementById('prev');
+	var next = document.getElementById('next');
+		var i = 1;
+
+		next.addEventListener('click', function() {
+			chair.src = './images/slider/' + images[i];
+			if (i < 2) {
+				i++;
+			} else {
+				i = 0;
+			}
+		});
+
+		prev.addEventListener('click', function() {
+			chair.src = './images/slider/' + images[i];
+			if (i > 0) {
+				i--;
+			} else {
+				i = 2;
+			}
+		});
+
+
 
 
 
